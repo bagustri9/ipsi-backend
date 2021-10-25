626f336e5f3caa772c0fff3009ab3f77ac201279
bagustri9 <57931683+bagustri9@users.noreply.github.com>
2021-10-25 21:55:55 +0700

    adding fitur barang

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/barang",[BarangController::class, 'index']);
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('abc');
 });
