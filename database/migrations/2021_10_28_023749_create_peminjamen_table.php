29d3693187c2db5e2640edd13a14113f5ae0827c
Wildanzr <graita.sukma@gmail.com>
2021-10-28 09:52:30 +0700

    tambah data

---
+++ database/migrations/2021_10_28_023749_create_peminjamen_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeminjamenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjamen', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("user_id");
+            $table->integer("total");
+            $table->string("barang_jaminan");
+            $table->date("tanggal_rental");
+            $table->date("rencana_pengembalian");
+            $table->integer("status");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjamen');
+    }
+}
