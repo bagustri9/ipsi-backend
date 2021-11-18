44ccf8a12bcec3875f8a3a28b5d2b3c48c54134f
bagustri9 <bagustriwidiyanto9@gmail.com>
2021-11-18 09:30:15 +0700

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -40,9 +46,9 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $user = User::where('email', $request -> email)->first();
-        if(!$user || !Hash::check($request->password,$user->password)){
-            return response ([
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
                 'msg' => 'failed to login'
             ], 401);
         }
@@ -64,5 +70,46 @@ class AuthController extends Controller
         return [
             'msg' => "logout"
         ];
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json(['status' => __($status)])
+            : response()->json(['status' => __($status), 'email' => __($status)]);
+    }
+
+    public function resetPasword(Request $request)
+    {
+
+
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? response()->json('status', __($status))
+            : response()->json(['status' => __($status), 'email' => __($status)]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,28 +19,30 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::group(['middleware' => ['cors']], function ($router) {
-    Route::post('/register',[AuthController::class,'register']);
-    Route::get("/barang/search_query={nama}",[BarangController::class, 'find']);
-    Route::get("/barang/{id}",[BarangController::class, 'show']);
-    Route::get("/barang/{id}/filter",[BarangController::class, 'filter']);
-    Route::get("/barang/{id}/delete",[BarangController::class, 'destroy']);
-    Route::post("/barang",[BarangController::class, 'store']);
-    Route::post("/barang/{id}/update",[BarangController::class, 'update']);
-    Route::get("/barang",[BarangController::class, 'index']);
-    Route::post("/login",[AuthController::class, 'login']);
-    Route::get("/barang",[BarangController::class, 'index']);
-    Route::get("/peminjamans",[PeminjamanController::class, 'index']);
-    Route::post("/peminjamans/{id}/update",[PeminjamanController::class, 'update']);
-    Route::get("/peminjamans/{id}/delete",[PeminjamanController::class, 'destroy']);
-    Route::post("/peminjaman",[PeminjamanController::class, 'store']);
-    Route::get("/peminjaman/date",[PeminjamanController::class, 'getAllDate']);
-    Route::get("/peminjaman/date/{date}",[PeminjamanController::class, 'getDate']);
-    Route::get("/peminjaman/{id}",[PeminjamanController::class, 'store']);
-    Route::get("/peminjaman/user/{user_id}",[PeminjamanController::class, 'peminjamanByUser']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get("/barang/search_query={nama}", [BarangController::class, 'find']);
+    Route::get("/barang/{id}", [BarangController::class, 'show']);
+    Route::get("/barang/{id}/filter", [BarangController::class, 'filter']);
+    Route::get("/barang/{id}/delete", [BarangController::class, 'destroy']);
+    Route::post("/barang", [BarangController::class, 'store']);
+    Route::post("/barang/{id}/update", [BarangController::class, 'update']);
+    Route::get("/barang", [BarangController::class, 'index']);
+    Route::post("/login", [AuthController::class, 'login']);
+    Route::get("/barang", [BarangController::class, 'index']);
+    Route::get("/peminjamans", [PeminjamanController::class, 'index']);
+    Route::post("/peminjamans/{id}/update", [PeminjamanController::class, 'update']);
+    Route::get("/peminjamans/{id}/delete", [PeminjamanController::class, 'destroy']);
+    Route::post("/peminjaman", [PeminjamanController::class, 'store']);
+    Route::get("/peminjaman/date", [PeminjamanController::class, 'getAllDate']);
+    Route::get("/peminjaman/date/{date}", [PeminjamanController::class, 'getDate']);
+    Route::get("/peminjaman/{id}", [PeminjamanController::class, 'store']);
+    Route::get("/peminjaman/user/{user_id}", [PeminjamanController::class, 'peminjamanByUser']);
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 });
-Route::group(['middleware' => ['cors','auth:sanctum']], function ($router) {
-    Route::get("/user/{id}",[UserController::class, 'show']);
-    Route::post("/user/{id}/update",[UserController::class, 'update']);
-    Route::post("/logout",[AuthController::class, 'logout']);
-    Route::post("/user/profile-image/{id}",[UserController::class, 'profileImage']);
+Route::group(['middleware' => ['cors', 'auth:sanctum']], function ($router) {
+    Route::get("/user/{id}", [UserController::class, 'show']);
+    Route::post("/user/{id}/update", [UserController::class, 'update']);
+    Route::post("/logout", [AuthController::class, 'logout']);
+    Route::post("/user/profile-image/{id}", [UserController::class, 'profileImage']);
 });
